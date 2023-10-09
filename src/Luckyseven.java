ff8bb6ed74d4ac26cac9ec9237e17e863ce53b02
Wasantha <wasanthahwr@gmail.com>
2023-10-09 20:34:29 +0530

    Generating random numbers and checking the inputs equal to seven

--- src/Luckyseven.java
+++ src/Luckyseven.java
@@ -1,9 +1,23 @@
+import java.util.Random;
 
 public class Luckyseven {
+    public static void main(String[] args) {
+        // Creating a random number for the game
+        Random random = new Random();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Generating 3 numbers from 1 to 10
+        int number1 = random.nextInt(10) + 1;
+        int number2 = random.nextInt(10) + 1;
+        int number3 = random.nextInt(10) + 1;
 
-	}
+        // Printing the random numbers
+        System.out.println("Random numbers: " + number1 + ", " + number2 + ", " + number3);
 
+        // Checking the input number is 7
+        if (number1 == 7 || number2 == 7 || number3 == 7) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost.");
+        }
+    }
 }
